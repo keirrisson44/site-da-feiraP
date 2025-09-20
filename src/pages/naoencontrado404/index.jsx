3002dbd2b86deda09e403523baeadc223cdd5f0d
keirrisson44 <ra24351835850@acaonsfatima.org.br>
2025-09-19 21:32:39 -0300

    sss

--- src/pages/naoencontrado404/index.jsx
+++ src/pages/naoencontrado404/index.jsx
@@ -6,7 +6,7 @@ export default function NaoEncontrado() {
     return(
         <div className='Pagina-NaoEncontrado'>
             <h1>pagina não encontrada</h1>
-         <img className='po' src='/assets/image/404.png'/>
+         <img className='po' src='/assets/image/404.png' alt='não encontrado'/>
         </div>
     )
 }
